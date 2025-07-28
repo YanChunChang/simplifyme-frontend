818d7639aa4029a9d5dc2f66ebb0a8a9d6041488
chun <yanchun.chang5@gmail.com>
2025-07-28 23:24:34 +0200

    wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CaptionComponent } from './components/caption/caption.component';
+import { ImageGeneratorComponent } from './components/image-generator/image-generator.component';
+import { SimplificationComponent } from './components/simplification/simplification.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'homepage', pathMatch: 'full' },
+    { path: 'homepage', component: HomepageComponent },
+    { path: 'caption', component: CaptionComponent },
+    { path: 'simplification', component: SimplificationComponent },
+    { path: 'imageGenerator', component: ImageGeneratorComponent },
+];
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule} from '@angular/router';
+
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
